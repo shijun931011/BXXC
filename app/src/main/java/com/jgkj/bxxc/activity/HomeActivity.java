4baf9a1d438ff9f54769430f325ff451dbd6872c
shijun931011 <2301855349@qq.com>
2017-05-22 23:36:39 +0800

    教练搜索

--- app/src/main/java/com/jgkj/bxxc/activity/HomeActivity.java
+++ app/src/main/java/com/jgkj/bxxc/activity/HomeActivity.java
@@ -85,16 +85,16 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
     private SchoolAction schoolAction;
     private Dialog dialog;
     //搜索按钮
-    private ImageView search;
+//    private ImageView search;
     private View inflate;
     private CarSendActivity carSendMap;
     // 定义一个变量，来标识是否退出
     private static boolean isExit = false;
     //判断调过来的Activity
     private String fromActivity = null;
-    private EditText mEtSearch = null;// 输入搜索内容
-    private Button mBtnClearSearchText = null;// 清空搜索信息的按钮
-    private LinearLayout mLayoutClearSearchText = null;
+//    private EditText mEtSearch = null;// 输入搜索内容
+//    private Button mBtnClearSearchText = null;// 清空搜索信息的按钮
+//    private LinearLayout mLayoutClearSearchText = null;
     //jPush推送
     private EditText msgText;
     public final static int CLOSE_ACTIVITY = 1001;
@@ -105,7 +105,7 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
     private UserInfo userInfo;
     private UserInfo.Result result;
     private String token;
-    private String searchUrl = "http://www.baixinxueche.com/index.php/Home/Apitoken/like";
+//    private String searchUrl = "http://www.baixinxueche.com/index.php/Home/Apitoken/like";
     private String versionUrl = "http://www.baixinxueche.com/index.php/Home/Apitoken/versionandroid";
 
     @Override
@@ -188,8 +188,8 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
         kefu = (ImageView) findViewById(R.id.remind);
         kefu.setOnClickListener(this);
         //搜索按钮
-        search = (ImageView) findViewById(R.id.search);
-        search.setOnClickListener(this);
+//        search = (ImageView) findViewById(R.id.search);
+//        search.setOnClickListener(this);
         car_frameLayout = (FrameLayout) findViewById(R.id.car_send_map);
         mCurrentFragment = index;
         frame = (FrameLayout) findViewById(R.id.index_fragment_layout);
@@ -237,7 +237,7 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
                 kefu.setVisibility(View.VISIBLE);
             } else if (fromActivity.equals("SimpleCoachActivity") || fromActivity.equals("IndexFragment")) {
 //            text_title.setText("报考");
-                search.setVisibility(View.VISIBLE);
+//                search.setVisibility(View.VISIBLE);
                 text_title.setVisibility(View.GONE);
                 radioButton2.setChecked(true);
                 scroll_bar.setVisibility(View.GONE);
@@ -255,66 +255,64 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
         transaction.addToBackStack(null);
         transaction.commit();
     }
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void creatDialog() {
-        dialog = new Dialog(this, R.style.ActionSheetDialogStyle);
-        // 填充对话框的布局
-        View inflate = LayoutInflater.from(this).inflate(R.layout.search_dialog,null);
-        // 初始化控件
-        mEtSearch = (EditText) inflate.findViewById(R.id.et_search);
-        mBtnClearSearchText = (Button) inflate.findViewById(R.id.btn_clear_search_text);
-        mLayoutClearSearchText = (LinearLayout) inflate.findViewById(R.id.layout_clear_search_text);
-        mEtSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before,
-                                      int count) {
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                int textLength = mEtSearch.getText().length();
-                if (textLength > 0) {
-                    mLayoutClearSearchText.setVisibility(View.VISIBLE);
-                } else {
-                    mLayoutClearSearchText.setVisibility(View.GONE);
-                }
-            }
-        });
-
-        mBtnClearSearchText.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mEtSearch.setText("");
-                mLayoutClearSearchText.setVisibility(View.GONE);
-            }
-        });
-        mEtSearch.setOnKeyListener(new View.OnKeyListener() {
-
-            @Override
-            public boolean onKey(View arg0, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                    search(mEtSearch.getText().toString().trim(), "1");
-                }
-                return false;
-            }
-        });
-        // 将布局设置给Dialog
-        dialog.setContentView(inflate);
-        // 获取当前Activity所在的窗体
-        Window dialogWindow = dialog.getWindow();
-        // 设置dialog横向充满
-        dialogWindow.setLayout(android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-                android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
-        // 设置Dialog从窗体中间弹出
-        dialogWindow.setGravity(Gravity.TOP);
-        dialog.show();/// 显示对话框
-    }
+//    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+//    public void creatDialog() {
+//        dialog = new Dialog(this, R.style.ActionSheetDialogStyle);
+//        // 填充对话框的布局
+//        View inflate = LayoutInflater.from(this).inflate(R.layout.search_dialog,null);
+//        // 初始化控件
+//        mEtSearch = (EditText) inflate.findViewById(R.id.et_search);
+//        mBtnClearSearchText = (Button) inflate.findViewById(R.id.btn_clear_search_text);
+//        mLayoutClearSearchText = (LinearLayout) inflate.findViewById(R.id.layout_clear_search_text);
+//        mEtSearch.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                int textLength = mEtSearch.getText().length();
+//                if (textLength > 0) {
+//                    mLayoutClearSearchText.setVisibility(View.VISIBLE);
+//                } else {
+//                    mLayoutClearSearchText.setVisibility(View.GONE);
+//                }
+//            }
+//        });
+//
+//        mBtnClearSearchText.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mEtSearch.setText("");
+//                mLayoutClearSearchText.setVisibility(View.GONE);
+//            }
+//        });
+//        mEtSearch.setOnKeyListener(new View.OnKeyListener() {
+//
+//            @Override
+//            public boolean onKey(View arg0, int keyCode, KeyEvent event) {
+//                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+//                    search(mEtSearch.getText().toString().trim(), "1");
+//                }
+//                return false;
+//            }
+//        });
+//        // 将布局设置给Dialog
+//        dialog.setContentView(inflate);
+//        // 获取当前Activity所在的窗体
+//        Window dialogWindow = dialog.getWindow();
+//        // 设置dialog横向充满
+//        dialogWindow.setLayout(android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+//                android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+//        // 设置Dialog从窗体中间弹出
+//        dialogWindow.setGravity(Gravity.TOP);
+//        dialog.show();/// 显示对话框
+//    }
 
     /**
      * 教练中心页面模糊查找
@@ -322,39 +320,39 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
      * @param str        编辑框输出的文字
      * @param searchPage 页数
      */
-    private void search(String str, String searchPage) {
-        OkHttpUtils
-                .post()
-                .url(searchUrl)
-                .addParams("input", str)
-                .addParams("page", searchPage)
-                .build()
-                .execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e, int i) {
-                        dialog.dismiss();
-                        Toast.makeText(HomeActivity.this, "网络异常，请检查网络！", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onResponse(String s, int i) {
-                        dialog.dismiss();
-                        Gson gson = new Gson();
-                        CoachDetailAction coachDetailAction = gson.fromJson(s, CoachDetailAction.class);
-                        if (coachDetailAction.getCode() == 200) {
-                            coach = new CoachFragment();
-                            fragmentManager = getSupportFragmentManager();
-                            Bundle bundle = new Bundle();
-                            bundle.putString("SEARCH", s);
-                            coach.setArguments(bundle);
-                            transaction = fragmentManager.beginTransaction();
-                            transaction.replace(R.id.car_send_map, coach).commit();
-                        } else {
-                            Toast.makeText(HomeActivity.this, coachDetailAction.getReason(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-    }
+//    private void search(String str, String searchPage) {
+//        OkHttpUtils
+//                .post()
+//                .url(searchUrl)
+//                .addParams("input", str)
+//                .addParams("page", searchPage)
+//                .build()
+//                .execute(new StringCallback() {
+//                    @Override
+//                    public void onError(Call call, Exception e, int i) {
+//                        dialog.dismiss();
+//                        Toast.makeText(HomeActivity.this, "网络异常，请检查网络！", Toast.LENGTH_LONG).show();
+//                    }
+//
+//                    @Override
+//                    public void onResponse(String s, int i) {
+//                        dialog.dismiss();
+//                        Gson gson = new Gson();
+//                        CoachDetailAction coachDetailAction = gson.fromJson(s, CoachDetailAction.class);
+//                        if (coachDetailAction.getCode() == 200) {
+//                            coach = new CoachFragment();
+//                            fragmentManager = getSupportFragmentManager();
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("SEARCH", s);
+//                            coach.setArguments(bundle);
+//                            transaction = fragmentManager.beginTransaction();
+//                            transaction.replace(R.id.car_send_map, coach).commit();
+//                        } else {
+//                            Toast.makeText(HomeActivity.this, coachDetailAction.getReason(), Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//                });
+//    }
 
     /**
      * 点击监听事件
@@ -369,7 +367,7 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
             // 底部导航栏监听
             case R.id.radio_button_01:
                 titlebar.setVisibility(View.VISIBLE);
-                search.setVisibility(View.GONE);
+//                search.setVisibility(View.GONE);
                 place.setVisibility(View.VISIBLE);
                 kefu.setVisibility(View.VISIBLE);
                 index = new IndexFragment();
@@ -395,7 +393,7 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
                 break;
             case R.id.radio_button_03:
                 titlebar.setVisibility(View.VISIBLE);
-                search.setVisibility(View.GONE);
+//                search.setVisibility(View.GONE);
                 kefu.setVisibility(View.GONE);
                 place.setVisibility(View.GONE);
                 study = new StudyFragment();
@@ -409,7 +407,7 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
                 break;
             case R.id.radio_button_04:
                 titlebar.setVisibility(View.VISIBLE);
-                search.setVisibility(View.GONE);
+//                search.setVisibility(View.GONE);
                 kefu.setVisibility(View.GONE);
                 place.setVisibility(View.GONE);
                 my_set = new My_Setting_Fragment();
@@ -429,9 +427,9 @@ public class HomeActivity extends FragmentActivity implements OnClickListener {
                     mCurrentFragment = my_set;
                 }
                 break;
-            case R.id.search:
-                creatDialog();
-                break;
+//            case R.id.search:
+//                creatDialog();
+//                break;
 
             case R.id.remind:
                 new CallDialog(HomeActivity.this,"0551-65555744").call();
--- app/src/main/java/com/jgkj/bxxc/fragment/CoachFragment.java
+++ app/src/main/java/com/jgkj/bxxc/fragment/CoachFragment.java
@@ -1,53 +1,81 @@
 package com.jgkj.bxxc.fragment;
 
+import android.annotation.TargetApi;
+import android.app.Dialog;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.jgkj.bxxc.R;
+import com.jgkj.bxxc.activity.HomeActivity;
+import com.jgkj.bxxc.bean.CoachDetailAction;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.ArrayList;
+
+import okhttp3.Call;
 
 public class CoachFragment extends Fragment implements View.OnClickListener{
     private ViewPager viewPager;
     private ArrayList<Fragment> fragmentList;
     private Button btn_coach;
     private Button btn_private;
+    private ImageView search;
+    private Fragment coach;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    private EditText mEtSearch = null;// 输入搜索内容
+    private Button mBtnClearSearchText = null;// 清空搜索信息的按钮
+    private LinearLayout mLayoutClearSearchText = null;
+    private Dialog dialog;
+    private View view;
+    private String searchUrl = "http://www.baixinxueche.com/index.php/Home/Apitoken/like";
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.activity_private_coach, container, false);
+        view = inflater.inflate(R.layout.activity_private_coach, container, false);
+        return view;
     }
-
-
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        viewPager = (ViewPager) getActivity().findViewById(R.id.viewPager);
-
-        btn_private = (Button) getActivity().findViewById(R.id.btn_private);
-        btn_coach = (Button) getActivity().findViewById(R.id.btn_coach);
-
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        btn_private = (Button) view.findViewById(R.id.btn_private);
+        btn_coach = (Button) view.findViewById(R.id.btn_coach);
+        search = (ImageView) view.findViewById(R.id.search);
+        Log.d("shijun","搜索ID:"+search);
+        search.setOnClickListener(this);
         btn_private.setOnClickListener(this);
         btn_coach.setOnClickListener(this);
-
         fragmentList = new ArrayList<Fragment>();
         fragmentList.add(new PrivateFragment());
         fragmentList.add(new CoachFragment2());
-
         viewPager.setOffscreenPageLimit(2);
-
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
 
             public int getCount() {
                 return fragmentList.size();
             }
-
             public Fragment getItem(int arg0) {
                 return fragmentList.get(arg0);
             }
@@ -81,18 +109,122 @@ public class CoachFragment extends Fragment implements View.OnClickListener{
         });
 
     }
+    public void creatDialog() {
+        dialog = new Dialog(getActivity(), R.style.ActionSheetDialogStyle);
+        // 填充对话框的布局
+        View inflate = LayoutInflater.from(getActivity()).inflate(R.layout.search_dialog,null);
+        // 初始化控件
+        mEtSearch = (EditText) inflate.findViewById(R.id.et_search);
+        mBtnClearSearchText = (Button) inflate.findViewById(R.id.btn_clear_search_text);
+        mLayoutClearSearchText = (LinearLayout) inflate.findViewById(R.id.layout_clear_search_text);
+        mEtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-    public void onClick(View arg0) {
+            }
 
-        switch (arg0.getId()) {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int textLength = mEtSearch.getText().length();
+                if (textLength > 0) {
+                    mLayoutClearSearchText.setVisibility(View.VISIBLE);
+                } else {
+                    mLayoutClearSearchText.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        mBtnClearSearchText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEtSearch.setText("");
+                mLayoutClearSearchText.setVisibility(View.GONE);
+            }
+        });
+        mEtSearch.setOnKeyListener(new View.OnKeyListener() {
+
+            @Override
+            public boolean onKey(View arg0, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    search(mEtSearch.getText().toString().trim(), "1");
+                }
+                return false;
+            }
+        });
+        // 将布局设置给Dialog
+        dialog.setContentView(inflate);
+        // 获取当前Activity所在的窗体
+        Window dialogWindow = dialog.getWindow();
+        // 设置dialog横向充满
+        dialogWindow.setLayout(android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+                android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+        // 设置Dialog从窗体中间弹出
+        dialogWindow.setGravity(Gravity.TOP);
+        dialog.show();/// 显示对话框
+    }
+
+
+    /**
+     * 教练中心页面模糊查找
+     *
+     * @param str        编辑框输出的文字
+     * @param searchPage 页数
+     */
+    private void search(String str, String searchPage) {
+        OkHttpUtils
+                .post()
+                .url(searchUrl)
+                .addParams("input", str)
+                .addParams("page", searchPage)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int i) {
+                        dialog.dismiss();
+                        Toast.makeText(getActivity(), "网络异常，请检查网络！", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onResponse(String s, int i) {
+                        dialog.dismiss();
+                        Gson gson = new Gson();
+                        CoachDetailAction coachDetailAction = gson.fromJson(s, CoachDetailAction.class);
+                        if (coachDetailAction.getCode() == 200) {
+                            coach = new CoachFragment();
+//                            fragmentManager = getActivity().getSupportFragmentManager();
+                            fragmentManager= getFragmentManager();
+                            Log.d("ddddd","fragmentManager:"+fragmentManager);
+                            Bundle bundle = new Bundle();
+                            bundle.putString("SEARCH", s);
+                            coach.setArguments(bundle);
+                            transaction = fragmentManager.beginTransaction();
+                            transaction.replace(R.id.car_send_map, coach).commit();
+                        } else {
+                            Toast.makeText(getActivity(), coachDetailAction.getReason(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
             case R.id.btn_private:
                 viewPager.setCurrentItem(0, true);
                 break;
             case R.id.btn_coach:
                 viewPager.setCurrentItem(1, true);
                 break;
-            default:
+            case R.id.search:
+                creatDialog();
                 break;
+//            default:
+//                break;
+
         }
     }
 
